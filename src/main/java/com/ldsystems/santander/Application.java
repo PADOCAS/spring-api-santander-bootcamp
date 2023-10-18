cb26ff0cad60a522984bf91c3dbbd7f9b550d0e6
Luciano Decio Neto <lucianodeciopacheco@gmail.com>
2023-10-18 11:22:14 -0300

    Liberando Alterações

--- src/main/java/com/ldsystems/santander/Application.java
+++ src/main/java/com/ldsystems/santander/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+// Configurar o Railway.toml para utilizar no servidor do Railway
 // Necessário para rodar em HTTPS (Servidor Railway por exemplo)																						
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "URL Servidor Default") })
 @SpringBootApplication
@@ -14,5 +15,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
